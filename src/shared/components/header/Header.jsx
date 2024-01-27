0f7a7a12dd014c3e6c16569d5d51888fb1523b2a
Tomas Jara <tomasignaciojd@gmail.com>
2024-01-27 12:45:04 -0300

    cambio de color aplicado

--- src/shared/components/header/Header.jsx
+++ src/shared/components/header/Header.jsx
@@ -3,7 +3,7 @@ import { CarroDeCompra } from '../carroDeCompra/CarroDeCompra'
 export const Header = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg bg-warning shadow-sm ">
                 <div className="container">
                     <a className="navbar-brand" href="/">Tienda</a>
                     <CarroDeCompra />
